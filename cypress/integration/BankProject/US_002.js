a130329c4353570ddfc9ce5a0a39314c377295cb
Arslnmrv <arslanmerve1619@gmail.com>
2022-01-19 19:00:25 +0300

    mr/vx

--- cypress/integration/BankProject/US_002.js
+++ cypress/integration/BankProject/US_002.js
@@ -1,1 +1,36 @@
-//
+Cypress.on('uncaught:exception', (err, runnable) => {
+   
+    return false
+  })
+  describe("Sistem gecersiz karakterlerle kayit yapilmasina izin vermez", () => {
+    it.skip("ssn", () => {
+        cy.visit("https://www.gmibank.com/account/register");
+        cy.get('#ssn').type('merve_')
+        cy.wait(3000)
+        cy.get('#firstname').click()
+       
+        cy.get('#ssn').should('have.attr', 'value')
+        //ssn '-' karakteri haric baska ozel karakterler kabul etmemeli 
+      
+    })  
+    it.skip("phone", () => {
+      cy.visit("https://www.gmibank.com/account/register");
+      cy.get('#mobilephone').type('555.555.5555')
+      cy.wait(3000)
+      cy.get('#username').click()
+     
+      //number '-' karakteri haric baska ozel karakterler kabul etmemeli
+    
+  })  
+  it.skip("email", () => {
+    cy.visit("https://www.gmibank.com/account/register");
+    cy.get('#email').type('merve#hotmail,com')
+    cy.wait(3000)
+    cy.get('#firstPassword').click()
+    cy.get(':nth-child(7) > .invalid-feedback').should('have.text','Your email is invalid.')
+   
+  
+    //email '@' ve '.' karakteri haric baska ozel karakterler kabul etmemeli 
+  
+})  
+  })
